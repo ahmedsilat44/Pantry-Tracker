d9f6664d9ec3d3b0628b885a1ae62edd20ee1eaf
Ahmed Silat <ahmedsilat44@gmail.com>
2024-08-05 16:55:35 +0500

    finish

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={{margin:0}} className={inter.className}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 'use client'
 
-import Image from "next/image";
+import * as React from 'react';
 import { firestore } from "@/firebase";
 import { useEffect, useState } from "react";
-import {Box, Stack, Typography, Button, Modal, TextField, Table, TableContainer, TableHead, TableBody, TableCell, TableRow, Paper, InputAdornment} from "@mui/material";
+import {ListItemButton, ListItemText, ListItem, List, Drawer, Box, Stack, Typography, Button, Modal, TextField, Table, TableContainer, TableHead, TableBody, TableCell, TableRow, Paper, InputAdornment, IconButton, Menu, MenuItem, Tabs} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import {getDocs, doc, query, collection, setDoc, deleteDoc, getDoc, get} from "firebase/firestore";
 
 const style = {
@@ -19,7 +20,17 @@ const style = {
   p: 4,
   display: 'flex',
   flexDirection: 'column',
-  gap: 3, 
+  gap: 3,
+}
+
+const style2 = {
+  
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100vh',
 }
 
 
@@ -32,8 +43,25 @@ export default function Home() {
   const [searchQuery, setSearchQuery] = useState('');
   const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState('');
-
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [recipes, setRecipes] = useState(false);
   const filteredInventory = inventory.filter( item => item.name.toLowerCase().includes(searchQuery))
+
+  const openMenu = Boolean(anchorEl);
+
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.target);
+  }
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  }
+
+  const updateRecipeValue = (e) => {
+    e.target.parentElement.innerText === "Inventory" ? setRecipes(false) :
+    setRecipes(true);
+  }
 
 
   
@@ -51,6 +79,16 @@ export default function Home() {
     updateInventory()
   }, [])
 
+  const editItem = async (item,data) => {
+    const docRef = doc(collection(firestore, 'pantry',"default_pantry", 'pantryItems'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
+    }
+    await updateInventory()
+  }
+
 
   const addItem = async (item) => {
     const docRef = doc(collection(firestore, 'pantry',"default_pantry", 'pantryItems'), item)
@@ -64,6 +102,7 @@ export default function Home() {
     const caloreJson = await caloreData.json()
     console.log(caloreJson)
     const docData = {
+      "name": item,
       "calories": caloreJson.items[0].calories,
       "serving_size": JSON.stringify(caloreJson.items[0].serving_size_g)+"g",
       "quantity": 1
@@ -92,132 +131,145 @@ export default function Home() {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
+  
 
 
   return (
-    <Box
-    // width="100vw"
-    width={'100%'}
-    height="100vh"
-    display={'flex'}
-    justifyContent={'center'}
-    flexDirection={'column'}
-    alignItems={'center'}
-    gap={2}
-  >
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Add Item
-        </Typography>
-        <Stack width="100%" direction={'row'} spacing={2}>
-          <TextField
-            id="outlined-basic"
-            label="Item"
-            variant="outlined"
-            fullWidth
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-          />
-          <Button
-            variant="outlined"
-            onClick={() => {
-              addItem(itemName)
-              setItemName('')
-              handleClose()
-            }}
-          >
-            Add
-          </Button>
-        </Stack>
-      </Box>
-    </Modal>
-    <Stack direction="row" spacing={2}>
-      <Button variant="contained" onClick={handleOpen}>
-        Add New Item
-      </Button>
-      <TextField sx={{width: 500,}} id="outlined-basic" label="Search" variant="outlined" value={searchQuery} onChange={(e) => {setSearchQuery(e.target.value)
-      }}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-      }}/>
-    </Stack>
+   <Stack  direction="row" sx={style2} backgroundColor="#fefefe"
+   
+   >
     
+   
+      <div >
       
-    <Box component={Paper} border={'1px solid #333'} elevation={3}  width={9/10} height={1/2} sx={{background: '#fff'}}>
-      
-      {/* <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
-        {filteredInventory.map(({name, quantity,calories,serving_size}) => (
-          <Box
-            key={name}
-            width="100%"
-            minHeight="150px"
-            display={'flex'}
-            justifyContent={'space-between'}
-            alignItems={'center'}
-            bgcolor={'#f0f0f0'}
-            paddingX={5}
-          >
-            <Typography variant={'p'} color={'#333'} textAlign={'center'}>
-              {name.charAt(0).toUpperCase() + name.slice(1)}
-            </Typography>
-            <Typography variant={'p'} color={'#333'} textAlign={'center'}> 
-              {calories} calories
-            </Typography>
-            <Typography variant={'p'} color={'#333'} textAlign={'center'}>Serving Size 
-              : {serving_size}
-            </Typography>
-            <Typography variant={'p'} color={'#333'} textAlign={'center'}>
-              Quantity: {quantity}
-            </Typography>
-            <Button variant="contained" onClick={() => removeItem(name)}>
-              Remove
-            </Button>
-          </Box>
-        ))}
-      </Stack> */}
-      <TableContainer border={'1px solid #333'} component={Paper} >
-      <Table  aria-label="simple table">
-      
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Name</TableCell>
-            <TableCell align="center">Quantity</TableCell>
-            <TableCell align="center">Calories</TableCell>
-            <TableCell align="center">Serving Size&nbsp;(g)</TableCell>
-            <TableCell align="center"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody height={1} >
-          {filteredInventory.map(({name, quantity,calories,serving_size}) => (
-            <TableRow
-              key={name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="center" component="th" scope="row">
-                {name}
-              </TableCell>
-              <TableCell align="center">{quantity}</TableCell>
-              <TableCell align="center">{calories}</TableCell>
-              <TableCell align="center">{serving_size}</TableCell>
-              <TableCell align="center"><Button variant="contained" onClick={() => removeItem(name)}>
-              Remove
-            </Button></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <Drawer
+      component={Paper}
+          sx={{
+            width: 240,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 240,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Box  role="presentation" >
+      <List>
+          <ListItem key={'Inventory'} disablePadding>
+            <ListItemButton onClick={updateRecipeValue}>
+              <ListItemText primary={'Inventory'} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={'Recipes'} disablePadding>
+            <ListItemButton onClick={updateRecipeValue}>
+              <ListItemText primary={'Recipes'} />
+            </ListItemButton>
+          </ListItem>
+      </List>
     </Box>
-  </Box>
+        </Drawer>
+      </div>
+      
+      <Stack sx={{ display: recipes ? "none" : "flex",
+        width: "100%",
+        height: "100%",
+      }} >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width="100%" direction={'row'} spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Item"
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      
+        <Stack direction="row" spacing={2} sx={{
+          padding: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+            <Button variant="contained" onClick={handleOpen}>
+              Add New Item
+            </Button>
+            <TextField sx={{width: "500px"}} id="outlined-basic" label="Search" variant="outlined" value={searchQuery} onChange={(e) => {setSearchQuery(e.target.value)
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}/>
+          </Stack>
+          <Stack height={"100%"} alignItems={"center"} justifyContent={"center"}>
+          <TableContainer component={Paper} sx={{width:"90%", height:"90%", border: 1, borderColor: "#000"}}>
+            <Table variant="outlined" aria-label="basic table" sx={{padding:10}} >
+                <TableHead sx={{background: "#f8f8f8"}}>
+                  <TableRow>
+                    <TableCell align="center">Name</TableCell>
+                    <TableCell align="center">Quantity</TableCell>
+                    <TableCell align="center">Calories</TableCell>
+                    <TableCell align="center">Serving Size&nbsp;(g)</TableCell>
+                    <TableCell align="center"></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody >
+                  {filteredInventory.map(({name, quantity,calories,serving_size}) => (
+                    <TableRow
+                      key={name}
+                      sx={{ height: 50 }}
+                    >
+                      <TableCell align="center" component="th" scope="row">
+                        {name}
+                      </TableCell>
+                      <TableCell align="center">{quantity}</TableCell>
+                      <TableCell align="center">{calories}</TableCell>
+                      <TableCell align="center">{serving_size}</TableCell>
+                      <TableCell align="center"><IconButton variant="contained" onClick={handleMenuClick} >
+                      <MoreHorizIcon/>
+                    </IconButton>
+                    <Menu anchorEl={anchorEl} open={openMenu} onClose={handleCloseMenu}>
+                      <MenuItem onClick={removeItem} >Remove</MenuItem>
+                      {/* <MenuItem >Edit</MenuItem> */}
+                    </Menu>
+                    </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            </Stack>
+        </Stack>
+
+      <Stack sx={{display: recipes ? null : "none"}}>
+        <Typography variant="h4" align="center">Recipes Coming Soon</Typography>
+      </Stack>
+      </Stack>
   );
 }
